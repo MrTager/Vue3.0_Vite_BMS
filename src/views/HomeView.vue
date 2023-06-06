4203ce5bf3bba3208ccccb4f1b3b3db891103298
MrTager <zhanghui@fline88.com>
2023-06-06 19:20:43 +0800

    update

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      上
+    </div>
+    <div>
+      <div>
+        左
+      </div>
+      <div>
+        中
+      </div>
+      <div>
+        右
+      </div>
+    </div>
+  </div>
+</template>
